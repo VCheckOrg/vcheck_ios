b59bfaca60e4ca656b76d1988e5b784306f3b918
@perets <perets@involve.software>
2022-04-28 12:52:56 +0300

    Country list

--- VcheckSDKDemoIOS/choose_country_stage/CountryListViewController.swift
+++ VcheckSDKDemoIOS/choose_country_stage/CountryListViewController.swift
@@ -8,7 +8,38 @@
 import Foundation
 import UIKit
 
-class ChooseCountryListViewController : UIViewController {
+class CountryListViewController : UIViewController {
+    
+    @IBOutlet var countryListTable: UITableView!
+    
+    let countries = [
+        "Украина", "Англия", "Франция", "Италия", "Германия", "Украина", "Англия", "Франция", "Италия", "Германия", "Англия", "Франция", "Италия", "Германия"]
     
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countryListTable.delegate = self
+        countryListTable.dataSource = self
+    }
 }
+
+extension CountryListViewController: UITableViewDelegate {
+    func tableView(_ countryListTable: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(countries[indexPath.row])
+    }
+}
+
+extension CountryListViewController: UITableViewDataSource {
+    func tableView(_ countryListTable: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+
+    func tableView(_ countryListTable: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = countryListTable.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = countries[indexPath.row]
+        cell.textLabel?.textColor = UIColor.white
+        return cell
+    }
+}
+
