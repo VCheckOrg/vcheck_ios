de697c624726567fbdb88476703b6c1ba36dcfc4
kaun <kaun@involve.software>
2022-05-03 20:46:09 +0300

    added more logic for take-photo view controller added Localizable.strings files added some UI changes and gr. resources

--- VcheckSDKDemoIOS/presentation/photo_upload_stage/TakeDocPhotoViewController.swift
+++ VcheckSDKDemoIOS/presentation/photo_upload_stage/TakeDocPhotoViewController.swift
@@ -14,28 +14,30 @@ class TakeDocPhotoViewController : UIViewController,
     
     
     @IBOutlet weak var firstPhotoCard: RoundedView!
-    
     @IBOutlet weak var secondPhotoCard: RoundedView!
     
     
     @IBOutlet weak var firstPhotoButton: RoundedView!
-    
     @IBOutlet weak var secondPhotoButton: RoundedView!
     
     
-    @IBOutlet weak var imgViewIconFirst: UIImageView!
+    @IBOutlet weak var docPhotoFirstImgHolder: UIImageView!
+    @IBOutlet weak var docPhotoSecondImgHolder: UIImageView!
     
+    
+    @IBOutlet weak var imgViewIconFirst: UIImageView!
     @IBOutlet weak var imgViewIconSecond: UIImageView!
     
     
     @IBOutlet weak var deleteFirstPhotoBtn: UIImageView!
-    
     @IBOutlet weak var deleteSecondPhotoBtn: UIImageView!
     
     
     @IBOutlet weak var tvFirstCardTitle: UILabel!
-    
     @IBOutlet weak var tvSecondCardTitle: UILabel!
+    
+    
+    @IBOutlet weak var btnContinueToPreview: UIButton!
     
     
     var selectedDocType: DocType? = nil
@@ -48,26 +50,70 @@ class TakeDocPhotoViewController : UIViewController,
     
     override func viewDidLoad() {
         
-        self.selectedDocType = DocType.docCategoryIdxToType(categoryIdx: KeychainHelper.shared.getSelectedDocTypeWithData()!.category!)
+        let docTypeWithData: DocTypeData = KeychainHelper.shared.getSelectedDocTypeWithData()!
+        
+        self.selectedDocType = DocType.docCategoryIdxToType(categoryIdx: docTypeWithData.category!)
+        
+        btnContinueToPreview.tintColor = UIColor(named: "borderColor")
+        btnContinueToPreview.titleLabel?.textColor = UIColor.gray
+        btnContinueToPreview.gestureRecognizers?.forEach(btnContinueToPreview.removeGestureRecognizer)
+        
+        deleteFirstPhotoBtn.isHidden = true
+        deleteSecondPhotoBtn.isHidden = true
         
         if (self.selectedDocType == DocType.INNER_PASSPORT_OR_COMMON) {
+            
+            firstPhotoCard.isHidden = false
+            secondPhotoCard.isHidden = false
+            
+            imgViewIconFirst.isHidden = true
+            imgViewIconSecond.isHidden = true
+            
+            tvFirstCardTitle.text = NSLocalizedString("photo_upload_title_common_forward", comment: "")
+            tvSecondCardTitle.text = NSLocalizedString("photo_upload_title_common_back", comment: "")
             
             setClickListenerForFistPhotoBtn()
             setClickListenerForSecondPhotoBtn()
         }
         if (self.selectedDocType == DocType.FOREIGN_PASSPORT) {
+            
+            firstPhotoCard.isHidden = false
             secondPhotoCard.isHidden = true
             
+            imgViewIconFirst.isHidden = false
+            imgViewIconSecond.isHidden = true
+            
+            tvFirstCardTitle.text = NSLocalizedString("photo_upload_title_foreign", comment: "")
+            
+            if (docTypeWithData.country == "ua") {
+                imgViewIconFirst.image = UIImage(imageLiteralResourceName: "doc_ua_international_passport")
+            } else {
+                imgViewIconFirst.isHidden = true
+            }
+
             setClickListenerForFistPhotoBtn()
         }
         if (self.selectedDocType == DocType.ID_CARD) {
             
+            firstPhotoCard.isHidden = false
+            secondPhotoCard.isHidden = false
+            
+            if (docTypeWithData.country == "ua") {
+                imgViewIconFirst.isHidden = false
+                imgViewIconSecond.isHidden = false
+                imgViewIconFirst.image = UIImage(imageLiteralResourceName: "doc_id_card_front")
+                imgViewIconSecond.image = UIImage(imageLiteralResourceName: "doc_id_card_back")
+            } else {
+                imgViewIconFirst.isHidden = true
+                imgViewIconSecond.isHidden = true
+            }
+            
+            tvFirstCardTitle.text = NSLocalizedString("photo_upload_title_id_card_forward", comment: "")
+            tvSecondCardTitle.text = NSLocalizedString("photo_upload_title_id_card_back", comment: "")
+ 
             setClickListenerForFistPhotoBtn()
             setClickListenerForSecondPhotoBtn()
         }
-        
-        
-        
     }
     
     func setClickListenerForFistPhotoBtn() {
@@ -85,6 +131,18 @@ class TakeDocPhotoViewController : UIViewController,
     }
     
     
+    @objc func handleTap(recognizer: PhotoUploadTapGesture) {
+        print(recognizer.photoTakeCase)
+        self.currentPhotoTakeCase = recognizer.photoTakeCase
+        switch(self.currentPhotoTakeCase) {
+            case PhotoTakeCase.FIRST:
+                takePhoto(recognizer)
+            case PhotoTakeCase.SECOND:
+                takePhoto(recognizer)
+            default: print("NO PHOTO TAKE CASE WAS SET!")
+        }
+    }
+    
     @objc func takePhoto(_ sender:UITapGestureRecognizer) {
         let vc = UIImagePickerController()
         vc.sourceType = .camera
@@ -101,22 +159,64 @@ class TakeDocPhotoViewController : UIViewController,
             print("No image found")
             return
         }
+        
+        if (currentPhotoTakeCase == PhotoTakeCase.FIRST) {
+            imgViewIconFirst.isHidden = true
+            tvFirstCardTitle.isHidden = true
+            deleteFirstPhotoBtn.isHidden = false
+            firstPhotoButton.isHidden = true
+            firstPhotoButton.gestureRecognizers?.forEach(firstPhotoButton.removeGestureRecognizer)
+            firstPhoto = image
+            docPhotoFirstImgHolder.image = firstPhoto
+        }
+        if (currentPhotoTakeCase == PhotoTakeCase.SECOND) {
+            imgViewIconSecond.isHidden = true
+            tvSecondCardTitle.isHidden = true
+            deleteSecondPhotoBtn.isHidden = false
+            secondPhotoButton.isHidden = true
+            secondPhotoButton.gestureRecognizers?.forEach(firstPhotoButton.removeGestureRecognizer)
+            secondPhoto = image
+            docPhotoSecondImgHolder.image = secondPhoto
+        }
+        
+        checkPhotoCompletenessAndSetProceedClickListener()
 
         print("--- TOOK A PHOTO SUCCESFULLY")
-        
-        
     }
     
-    @objc func handleTap(recognizer: PhotoUploadTapGesture) {
-        print(recognizer.photoTakeCase)
-        self.currentPhotoTakeCase = recognizer.photoTakeCase
-        switch(self.currentPhotoTakeCase) {
-            case PhotoTakeCase.FIRST:
-                takePhoto(recognizer)
-            case PhotoTakeCase.SECOND:
-                takePhoto(recognizer)
-            default: print("NO PHOTO TAKE CASE WAS SET!")
+    
+    func checkPhotoCompletenessAndSetProceedClickListener() {
+        if (selectedDocType == DocType.FOREIGN_PASSPORT) {
+            if (firstPhoto != nil) {
+                prepareForNavigation(conditionsSatisfied: false)
+            } else {
+                let errText = NSLocalizedString("error_make_at_least_one_photo", comment: "")
+                self.showToast(message: errText, seconds: 2.0)
+            }
+        } else if (selectedDocType == DocType.INNER_PASSPORT_OR_COMMON) {
+            if (firstPhoto != nil) {
+                prepareForNavigation(conditionsSatisfied: true)
+            } else if (secondPhoto != nil && firstPhoto == nil) {
+                firstPhoto = secondPhoto
+                secondPhoto = nil
+                prepareForNavigation(conditionsSatisfied: true)
+            } else if (secondPhoto != nil && firstPhoto != nil) {
+                prepareForNavigation(conditionsSatisfied: false)
+            } else {
+                let errText = NSLocalizedString("error_make_at_least_one_photo", comment: "")
+                self.showToast(message: errText, seconds: 2.0)
+            }
+        } else {
+            if (firstPhoto != nil && secondPhoto != nil) {
+                prepareForNavigation(conditionsSatisfied: true)
+            }
         }
+    }
+    
+    func prepareForNavigation(conditionsSatisfied: Bool) {
+        btnContinueToPreview.tintColor = UIColor(named: "Default")
+        btnContinueToPreview.titleLabel?.textColor = UIColor.white
+        
     }
 }
 
@@ -124,6 +224,7 @@ class PhotoUploadTapGesture: UITapGestureRecognizer {
     var photoTakeCase: PhotoTakeCase = PhotoTakeCase.NONE
 }
 
+//TODO: add photo (UImage) deletion logic
 class PhotoDeleteTapGesture: UITapGestureRecognizer {
     let photoIdx: Int = 0
 }
